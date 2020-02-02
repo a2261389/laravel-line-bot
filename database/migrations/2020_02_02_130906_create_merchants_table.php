d4432a563cdc62b929e5033dce6dcc9c8b0e5dd0
tong <newrtong@gmail.com>
2020-02-02 22:16:59 +0800

    [database] add migration merchants

---
+++ database/migrations/2020_02_02_130906_create_merchants_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMerchantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('merchants', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('line_user_id')->comment('LINE userId');
+            $table->unsignedInteger('minimum_order')->comment('最低消費');
+            $table->unsignedInteger('highest_order')->comment('最高消費');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('merchants');
+    }
+}
